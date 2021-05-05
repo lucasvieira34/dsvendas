d7f2f25aea656d9da19f7f5260bca18c7725b591
Lucas Teixeira <lucas.vieira@brmalls.com.br>
2021-05-05 15:35:13 -0300

    Domain Model, Database seed

---
+++ backend/src/main/java/com/lucasvieira/dsvendas/domain/Seller.java
@@ -0,0 +1,26 @@
+package com.lucasvieira.dsvendas.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_sellers")
+public class Seller {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "seller")
+    private List<Sale> sales = new ArrayList<>();
+
+}
